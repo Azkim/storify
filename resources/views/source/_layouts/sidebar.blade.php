3d3ff1cf3fd9a01301bf47455086775dab16daf4
moshki <azkimm40@gmail.com>
2022-03-14 19:22:39 +0300

    rebuilt all tables and made them fully responsive, fixed the file upload bug, optimised the sidebar logic.

--- resources/views/source/_layouts/sidebar.blade.php
+++ resources/views/source/_layouts/sidebar.blade.php
@@ -46,7 +46,7 @@
         </a>
         @else
         <a class="flex items-center mt-4 py-2 px-6 text-gray-500 hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100" href="{{ route('users.index') }}">
-        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z" />
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z" />
             </svg>
